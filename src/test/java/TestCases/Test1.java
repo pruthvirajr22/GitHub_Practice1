60e2b03abc19b4d54d6dd2d72b3dcd8c748ff32c
91968 <91968@pruthvi007>
2023-04-17 11:48:41 +0530

    created test1 class having 2 tests tc1 and tc2

---
+++ src/test/java/TestCases/Test1.java
@@ -0,0 +1,17 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+ @Test
+ public void tc1() 
+ {
+	 System.out.println("This is tc1 from Test1");
+ }
+ @Test
+ public void tc2() 
+ {
+	 System.out.println("This is tc2 from Test1");
+ }
+}
