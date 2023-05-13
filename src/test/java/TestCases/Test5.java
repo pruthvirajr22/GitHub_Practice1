bae14beaaef2e80371fcd3e62880b67dae67cca5
91968 <91968@pruthvi007>
2023-05-13 09:58:56 +0530

    this is test5

---
+++ src/test/java/TestCases/Test5.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+public class Test5 
+{
+ public static void main(String[] args) {
+	System.out.println("this is the test5");
+	
+
+}
+ public static void m1() 
+ {
+	 System.out.println("this is m1 from test5");
+ }
+}
